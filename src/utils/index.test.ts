c8dffae96b8af6b9106f0b6f431e235fc7572477
Endri Gjiri <egjiri@gmail.com>
2020-10-04 12:01:07 -0400

    Upgrade the @egjiri/node-kit package to access the new Cases testing helper

--- src/utils/index.test.ts
+++ src/utils/index.test.ts
@@ -1,12 +1,12 @@
+import { Cases } from '@egjiri/node-kit/testing/helpers';
 import { buildClassName } from '.';
 
 describe('test buildClassName function', () => {
-  const cases: [string, (string | undefined | null | false)[], string | undefined][] = [
+  const cases: Cases<typeof buildClassName> = [
     ['normal use case', ['card'], 'card'],
     ['boolean use case', [true && 'true', false && 'false'], 'true'],
     ['falsy use case', ['', undefined && 'undefined', null && null, false && false], undefined],
     ['multiple use cases', ['card', true && 'active', false && 'false'], 'card active'],
-
   ];
   test.each(cases)('%s', (_, args, expected) => {
     const actual = buildClassName(...args);
